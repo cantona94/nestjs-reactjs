7c57a36e5273646fa0dda499ddffbdfd8a46d157
Vladimir Staninov <52317262+cantona94@users.noreply.github.com>
2023-07-25 13:29:06 +0500

    add jwtStrategy, jwtGuard, fix authorization

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
+
+  @Post('login')
+  @UseGuards(LocalAuthGuard)
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @Get('profile')
+  @UseGuards(JwtAuthGuard)
+  getProfile(@Request() req) {
+    return req.user;
+  }
 }
--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -11,9 +11,4 @@ export class UserController {
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
-
-  // @Get()
-  // findOne() {
-  //   return this.userService.findOne();
-  // }
 }
