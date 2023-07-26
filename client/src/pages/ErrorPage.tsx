3f4f3490008de368b5fc624b06d988c42c704518
Vladimir Staninov <52317262+cantona94@users.noreply.github.com>
2023-07-26 22:30:40 +0500

    add Header, router, errorPage

--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -1,8 +1,21 @@
 import { FC } from 'react'
+import img from '../assets/page_not_found.png'
+import { Link } from 'react-router-dom'
 
 const ErrorPage: FC = () => {
   return (
-    <div>ErrorPage</div>
+    <div
+      className='min-h-screen bg-slate-900 font-roboto text-white 
+      flex justify-center items-center flex-col gap-10'
+    >
+      <img src={img} alt="img" className='h-96' />
+      <Link
+        className='bg-sky-500 rounded-md px-6 py-2 hover:bg-sky-600'
+        to={'/'}
+      >
+        Back
+      </Link>
+    </div>
   )
 }
 
