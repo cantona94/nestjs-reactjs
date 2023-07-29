16813f137732a7663d2db494d7dbaeaa44c32913
Vladimir Staninov <52317262+cantona94@users.noreply.github.com>
2023-07-29 15:58:13 +0500

    add CategoryModal

---
+++ client/src/components/CategoryModal.tsx
@@ -0,0 +1,37 @@
+import { FC } from 'react';
+import { Form } from 'react-router-dom';
+
+interface IProps {
+  id: number;
+  type: 'post' | 'patch';
+  setVisibleModal: (visible: boolean) => void;
+}
+
+const CategoryModal: FC<IProps> = ({ id, type, setVisibleModal }) => {
+  return (
+    <div
+      className="fixed top-0 left-0 bottom-0 right-0 w-full h-full 
+    bg-black/50 flex justify-center items-center"
+    >
+      <Form className="grid w-[300px] gap-2 rounded-md bg-slate-900 p-5">
+        <label htmlFor="title">
+          <small>Category Title</small>
+          <input
+            className="input w-full"
+            type="text"
+            name="title"
+            placeholder="Title..."
+          />
+        </label>
+        <div className="flex items-center gap-2">
+          <button className="btn btn-green" type="submit">
+            {type === 'patch' ? 'Save' : 'Create'}
+          </button>
+          <button className="btn btn-red">Close</button>
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+export default CategoryModal;
--- client/src/pages/Categories.tsx
+++ client/src/pages/Categories.tsx
@@ -2,41 +2,45 @@ import { FC } from 'react';
 import { AiFillEdit, AiFillCloseCircle } from 'react-icons/ai';
 import { FaPlus } from 'react-icons/fa';
 import { Form } from 'react-router-dom';
+import CategoryModal from '../components/CategoryModal';
 
 const Categories: FC = () => {
   return (
-    <div className="mt-10 rounded-md bg-slate-800 p-4">
-      <h1>Your category list:</h1>
-      <div className="mt-2 flex flex-wrap items-center gap-2">
-        <div
-          className="group relative flex items-center gap-2 rounded-lg
-         bg-blue-600 px-4 py-2"
-        >
-          Salry
+    <>
+      <div className="mt-10 rounded-md bg-slate-800 p-4">
+        <h1>Your category list:</h1>
+        <div className="mt-2 flex flex-wrap items-center gap-2">
           <div
-            className="absolute px-3 left-0 top-0 bottom-0 right-0 hidden
-            rounded-lg bg-black/90 items-center justify-between group-hover:flex"
+            className="group relative flex items-center gap-2 rounded-lg
+         bg-blue-600 px-4 py-2"
           >
-            <button>
-              <AiFillEdit />
-            </button>
-            <Form className="flex" method="delete" action="/categories">
-              <input type="hidden" value={'Category ID'} />
-              <button type="submit">
-                <AiFillCloseCircle />
+            Salry
+            <div
+              className="absolute px-3 left-0 top-0 bottom-0 right-0 hidden
+            rounded-lg bg-black/90 items-center justify-between group-hover:flex"
+            >
+              <button>
+                <AiFillEdit />
               </button>
-            </Form>
+              <Form className="flex" method="delete" action="/categories">
+                <input type="hidden" value={'Category ID'} />
+                <button type="submit">
+                  <AiFillCloseCircle />
+                </button>
+              </Form>
+            </div>
           </div>
         </div>
-      </div>
-      <button
-        className="mt-5 flex max-w-fit items-center gap-2
+        <button
+          className="mt-5 flex max-w-fit items-center gap-2
       text-white/50 hover:text-white"
-      >
-        <FaPlus />
-        <span>Create a new category</span>
-      </button>
-    </div>
+        >
+          <FaPlus />
+          <span>Create a new category</span>
+        </button>
+      </div>
+      <CategoryModal id={1} type="post" setVisibleModal={() => {}} />
+    </>
   );
 };
 
